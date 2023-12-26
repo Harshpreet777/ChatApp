05c3b6bc92f6232f957dc03acab734b31c44ca8e
Harshpreet Singh <harshpreet.singh@successive.com>
2023-12-26 15:32:59 +0530

    26/12/23

--- lib/screens/chatpage.dart
+++ lib/screens/chatpage.dart
@@ -18,7 +18,9 @@ class _ChatPageClassState extends State<ChatPageClass> {
   @override
   void initState() {
     super.initState();
-    streamController = StreamController();
+    streamController = StreamController(
+      onListen: () {},
+    );
   }
 
   @override
@@ -41,89 +43,85 @@ class _ChatPageClassState extends State<ChatPageClass> {
           backgroundColor: const Color(0xff273443),
         ),
         backgroundColor: const Color.fromARGB(255, 29, 38, 47),
-        body: chatbuild());
-  }
-
-  Column chatbuild() {
-    return Column(
-        children: [
-          Expanded(
-            child: StreamBuilder<String>(
-              stream: streamController.stream,
-              builder: (context, snapshot) {
-                if (snapshot.hasData) {
-                  if (snapshot.data == '') {
-                    message2 = [];
-                  }
-                  return ListView.builder(
-                    reverse: true,
-                    shrinkWrap: true,
-                    itemCount: message2.length,
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: const EdgeInsets.all(16.0),
-                        child: Align(
-                            alignment: BooleanClass.isSender == false
-                                ? Alignment.topRight
-                                : Alignment.topLeft,
-                            child: ClipRRect(
-                              borderRadius:
-                                  const BorderRadius.all(Radius.circular(12)),
-                              child: Container(
-                                  padding: const EdgeInsets.all(15),
-                                  color: Colors.blue,
+        body: Column(
+          children: [
+            Expanded(
+              child: StreamBuilder<String>(
+                stream: streamController.stream,
+                builder: (context, snapshot) {
+                  if (snapshot.hasData) {
+                    return ListView.builder(
+                      shrinkWrap: true,
+                      reverse: true,
+                      itemCount: message2.length,
+                      itemBuilder: (context, index) {
+                        if (message2[index].isEmpty) {
+                          return Container();
+                        } else {
+                          return Padding(
+                            padding: const EdgeInsets.all(16.0),
+                            child: Align(
+                                alignment: BooleanClass.isSender == false
+                                    ? Alignment.topRight
+                                    : Alignment.topLeft,
+                                child: ClipRRect(
+                                  borderRadius: const BorderRadius.all(
+                                      Radius.circular(12)),
                                   child: Flexible(
                                     fit: FlexFit.tight,
-                                    child: Text(
-                                      message2[index].toString(),
-                                      style: TextStyle(
-                                          color: ColorConstant.white,
-                                          fontSize: 20),
-                                    ),
-                                  )),
-                            )),
-                      );
-                    },
-                  );
-                } else {
-                  return Container();
-                }
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: TextFormField(
-              textInputAction: TextInputAction.send,
-              onEditingComplete: () {
-                setState(() {
-                  message2.add(myController.text);
-                  for (int i = 0; i < message2.length; i++) {
-                    streamController.sink.add(message2[i].toString());
+                                    child: Container(
+                                        padding: const EdgeInsets.all(15),
+                                        color: Colors.blue,
+                                        child: Text(
+                                          message2[index].toString(),
+                                          style: TextStyle(
+                                              color: ColorConstant.white,
+                                              fontSize: 20),
+                                        )),
+                                  ),
+                                )),
+                          );
+                        }
+                      },
+                    );
+                  } else {
+                    return Container();
                   }
-                  myController.clear();
-                });
-              },
-              onTap: () {
-                setState(() {
-                  message2.add(myController.text);
-                  for (int i = 0; i < message2.length; i++) {
-                    streamController.sink.add(message2[i].toString());
-                  }
-                  myController.clear();
-                });
-              },
-              controller: myController,
-              decoration: InputDecoration(
-                  suffixIcon: Icon(
-                    Icons.send,
-                    color: ColorConstant.white,
-                  ),
-                  border: const OutlineInputBorder()),
-              style: TextStyle(color: ColorConstant.white),
+                },
+              ),
             ),
-          )
-        ],
-      );
+            Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: TextFormField(
+                onEditingComplete: () {
+                  setState(() {
+                    message2.add(myController.text);
+                    for (int i = 0; i < message2.length; i++) {
+                      streamController.sink.add(message2[i].toString());
+                    }
+                    myController.clear();
+                  });
+                },
+                onTap: () {
+                  setState(() {
+                    message2.add(myController.text);
+                    for (int i = 0; i < message2.length; i++) {
+                      streamController.sink.add(message2[i].toString());
+                    }
+                    myController.clear();
+                  });
+                },
+                controller: myController,
+                decoration: InputDecoration(
+                    suffixIcon: Icon(
+                      Icons.send,
+                      color: ColorConstant.white,
+                    ),
+                    border: const OutlineInputBorder()),
+                style: TextStyle(color: ColorConstant.white),
+              ),
+            )
+          ],
+        ));
   }
 }
